d0957f29e84a01862778153ad634cfd5755e2866
Manuel Schmid <manuel.schmid@odt.net>
2023-01-22 18:26:54 +0100

    refactor: rename index route response

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Traceroute Server')
 })
 
 server.post('/traceroute', (req, res) => {
